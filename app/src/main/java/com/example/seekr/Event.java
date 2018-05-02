ec815722cbd893a04fffc39031a93892836d1e06
Chase <esmeraldastan@users.noreply.github.com>
2018-05-02 14:03:54 -0700

    save

--- app/src/main/java/com/example/seekr/Event.java
+++ app/src/main/java/com/example/seekr/Event.java
@@ -1,35 +1,18 @@
 package com.example.seekr;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Event.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Event#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Event extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
 
     public Event() {
@@ -46,32 +29,16 @@ public class Event extends Fragment {
      * @return A new instance of fragment Event.
      */
     // TODO: Rename and change types and number of parameters
-    public static Event newInstance(String param1, String param2) {
-        Event fragment = new Event();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
 
 
+
+   /** Button rk;
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    Button rk;
-    @Override
-    public void onCreate (Bundle savedInstanceState){
+        super.onCreate();
         setContentView(R.layout.activity_main);
-        rk=(Button)findViewById(R.id.button);
+        rk = (Button) findViewById(R.id.button);
         rk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,56 +47,11 @@ public class Event extends Fragment {
                 String shareBody = "Your Body Here";
                 String shareSub = "Your Subject here";
                 myIntent.putExtra(Intent.EXTRA_SUBJECT, shareBody);
-                myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
-                startActivity(Intent.createChooser(myIntent,"share using"));
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(myIntent, "share using"));
 
             }
         });
-    }
-
-
-
-
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(com.example.seekr.R.layout.fragment_event, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-       
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+    }*/
 }
+
